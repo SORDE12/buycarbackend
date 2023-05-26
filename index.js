8ebde962f5304a7d96634dbb87e1fae1578f4ebd
SORDE12 <sordeomkar2564@gmail.com>
2023-05-27 02:15:21 +0530

    added

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const { connection } = require("./config/db");
 
 let express = require("express");
-let cors=require("cors")
+let cors = require("cors");
 let { dealerRouter } = require("./routes/Dealer.route");
-let {carRouter} = require("./routes/Car.route");
+let { carRouter } = require("./routes/Car.route");
 const { authanticate } = require("./middleware/authantification");
 let app = express();
-require('dotenv').config()
-app.use(cors({
-  origin:"*"
-}))
+require("dotenv").config();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("this is car page");
+});
 
 app.use("/dealers", dealerRouter);
-app.use(authanticate)
- app.use("/car", carRouter);
 
- app.get('/',(req,res)=>{
-   res.send("this is car page")
- })
+app.use(authanticate);
+app.use("/car", carRouter);
 
 app.listen(process.env.port, async () => {
   try {
