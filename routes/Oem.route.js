dc818f12bb596106852dbc7edfe4ba3695cafe1f
SORDE12 <sordeomkar2564@gmail.com>
2023-05-29 11:59:59 +0530

    added

--- routes/Oem.route.js
+++ routes/Oem.route.js
@@ -5,9 +5,9 @@ const { OemModal } = require("../model/oem.model");
 
 
 newCarRouter.get("/", async (req, res) => {
-    let query = req.query;
+   
     try {
-      const users = await OemModal.find(query);
+      const users = await OemModal.find();
       res.send(users);
     } catch (err) {
       res.send({ msg: "cannot get users", err: err.message });
